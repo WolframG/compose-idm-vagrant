06b34f60ed3271503d115bcca2a7bdaf5b45cee8
nopbyte <juandabytes@gmail.com>
2014-12-11 11:47:54 +0100

    switch to compile the review version

--- compose-idm/build/compile.sh
+++ compose-idm/build/compile.sh
@@ -3,6 +3,7 @@
 rm -r bin
 rm -fr compose-idm
 git clone https://github.com/nopobyte/compose-idm
+git checkout review
 cp ./files/*.properties compose-idm/src/main/resources/
 cd ./compose-idm
 #sed 's/gradle /\/vagrant\/gradle-2.1\/bin\/gradle /' compile_war.sh>compile_war_vagrant.sh
